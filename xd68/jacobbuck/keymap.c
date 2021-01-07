57b8d97235a70f8f70e3b2de3cef56f1c730c233
Jacob Buck <buck.jacob@gmail.com>
2021-01-07 00:45:51 +0000

    Add backlight fn keys to xd68 layout

--- xd68/jacobbuck/keymap.c
+++ xd68/jacobbuck/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------------------------------------.
      * |~ `| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
      * |----------------------------------------------------------------|
-     * |     |   |B- |B+ |   |   |   |Prv|Ply|Nxt|Mut|V- |V+ |Reset| MC |
+     * |     |Br-|Br+|   |BlT|BlS|   |Prv|Ply|Nxt|Mut|V- |V+ |Reset| MC |
      * |----------------------------------------------------------------|
      * |      |   |   |   |Find|   |   |   |   |   |   |   |       | AW |
      * |----------------------------------------------------------------|
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_FN] = LAYOUT_65_ansi(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS,
-        XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, RESET, _______,
+        XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, BL_TOGG, BL_STEP, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, RESET, _______,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_FIND, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
         _______, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, _______,
         _______, _______, _______, XXXXXXX, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
